8213879c37d0e834e5a68c2b1e185f3fc5e79f37
BasedUser <konstttantin@gmail.com>
2020-08-24 13:08:14 +0300

    Add *nix support, compactify and modernize code

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -7,94 +7,41 @@ import java.io.*;
 
 public class Parser{
     public static void main(String[] args) {
-    try{
-    String baka;
-    FileWriter writer0 = new FileWriter("output0.txt", false);
-    FileWriter writer1 = new FileWriter("output1.txt", false);
-    FileWriter writer2 = new FileWriter("output2.txt", false);
-    FileWriter writer3 = new FileWriter("output3.txt", false);
-    FileWriter writer4 = new FileWriter("output4.txt", false);
-    FileWriter writer5 = new FileWriter("output5.txt", false);
-    FileWriter writer6 = new FileWriter("output6.txt", false);
-    
-    
-    BufferedImage bi = ImageIO.read( new File( "image.png" ) );
-    int w=bi.getWidth();
-    int h=bi.getHeight();
-    int[][] pictureR=new int[w][h];
-    int[][] pictureG=new int[w][h];
-    int[][] pictureB=new int[w][h];
-    int counter=0;
-    
-    writer0.write("\r");writer2.write("\r");writer1.write("\r");
-    int x=0;int y=0;
-while(x<w){
-y=0;
-while(y<h){
-int rgb=bi.getRGB(x,y);
-pictureR[x][y] = (rgb >> 16) & 0xFF; 
-pictureG[x][y] = (rgb >> 8) & 0xFF; 
-pictureB[x][y] = (rgb ) & 0xFF;
-baka="draw rect "+x+" "+(h-y)+" 1 1 0\n";
-if(counter<1001){
-writer0.append(baka);
-baka="draw color "+pictureR[x][y]+" "+pictureG[x][y]+" "+pictureB[x][y]+" 0 0\n";
-writer0.append(baka);
-baka="drawflush @0\n";
-writer0.append(baka);}
-
-if(counter>1000&&counter<2001){
-writer1.append(baka);
-baka="draw color "+pictureR[x][y]+" "+pictureG[x][y]+" "+pictureB[x][y]+" 0 0\n";
-writer1.append(baka);
-baka="drawflush @0\n";
-writer1.append(baka);}
-
-if(counter>2000&&counter<3001){
-writer2.append(baka);
-baka="draw color "+pictureR[x][y]+" "+pictureG[x][y]+" "+pictureB[x][y]+" 0 0\n";
-writer2.append(baka);
-baka="drawflush @0\n";
-writer2.append(baka);}
-
-if(counter<4001&&counter>3000){
-writer3.append(baka);
-baka="draw color "+pictureR[x][y]+" "+pictureG[x][y]+" "+pictureB[x][y]+" 0 0\n";
-writer3.append(baka);
-baka="drawflush @0\n";
-writer3.append(baka);}
-
-if(counter>4000&&counter<5001){
-writer4.append(baka);
-baka="draw color "+pictureR[x][y]+" "+pictureG[x][y]+" "+pictureB[x][y]+" 0 0\n";
-writer4.append(baka);
-baka="drawflush @0\n";
-writer4.append(baka);}
-
-if(counter>5000&&counter<6001){
-writer5.append(baka);
-baka="draw color "+pictureR[x][y]+" "+pictureG[x][y]+" "+pictureB[x][y]+" 0 0\n";
-writer5.append(baka);
-baka="drawflush @0\n";
-writer5.append(baka);}
-if(counter>6000){
-writer6.append(baka);
-baka="draw color "+pictureR[x][y]+" "+pictureG[x][y]+" "+pictureB[x][y]+" 0 0\n";
-writer6.append(baka);
-baka="drawflush @0\n";
-writer6.append(baka);}
-counter++;
-y++;
+        try{
+            String curline;
+            FileWriter interf;
+            BufferedImage bi = ImageIO.read( new File( "image.png" ) );
+            int w=bi.getWidth();
+            int h=bi.getHeight();
+            int[][] pictureR=new int[w][h];
+            int[][] pictureG=new int[w][h];
+            int[][] pictureB=new int[w][h];
+            int pixels=0;
+            int x=0; int y=0;
+            int filesWritten = 0;
+            interf = new FileWriter("output0.txt", false);
+            while(x<w){
+                y=0;
+                while(y<h){
+                    int rgb=bi.getRGB(x,y);
+                    pictureR[x][y] = (rgb >> 16) & 0xFF; 
+                    pictureG[x][y] = (rgb >> 8) & 0xFF; 
+                    pictureB[x][y] = (rgb ) & 0xFF;
+                    if( (pixels / 999) > ((pixels-1) / 999)) {
+                        interf.append("drawflush display1\n");
+                        interf.flush();
+                        filesWritten++;
+                        interf = new FileWriter("output"+String.valueOf(filesWritten)+".txt");
+                    }
+                    interf.append("draw color "+pictureR[x][y]+" "+pictureG[x][y]+" "+pictureB[x][y]+" 255 0\n");
+                    interf.append("draw rect "+x+" "+(h-y)+" 1 1 0\n"); // two appends, for better readability
+                    // side note, the reason there's h-y is because Mindustry starts its origin in the *bottom left* instead of top left, like BufferedImage.
+                    // that's why we have to do this madness
+                    pixels++;
+                    y++;
+                }
+                x++;
+            }
+        }catch(IOException e){};
+    }
 }
-x++;
-}
-writer0.flush();
-writer1.flush();
-writer2.flush();
-if(counter<2134){System.out.println("1");}
-if(counter>2133&&counter<4267){System.out.println("2");}
-if(counter>4266){System.out.println("3");}
-}catch(IOException e){};
-
-
-}}
